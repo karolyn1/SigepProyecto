dffd0c3ef60942e013254e80a0cfff40251aa39e
WIN <WIN@WINPC>
2025-07-19 16:58:01 -0600

    Inicio de modulo bolda empleo, avance de vacantes disponibles

---
+++ Sigep.UI/Controllers/BolsaEmpleoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sigep.UI.Controllers
+{
+    public class BolsaEmpleoController : Controller
+    {
+        // GET: BolsaEmpleo
+        public ActionResult Vacantes()
+        {
+            return View();
+        }
+    }
+}
