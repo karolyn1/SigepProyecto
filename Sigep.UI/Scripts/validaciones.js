c50f17647b8824de264e712023efd1b1053d66d3
WIN <WIN@WINPC>
2025-07-21 20:27:51 -0600

    cambios en listavacante coordinador

--- Sigep.UI/Scripts/validaciones.js
+++ Sigep.UI/Scripts/validaciones.js
@@ -1,6 +1,8 @@
 ﻿$(document).ready(function () {
     if ($('#miTabla').length) {
         $('#miTabla').DataTable({
+            responsive: true,
+            autoWidth: false, // evita que DataTables fije anchos rígidos
             language: {
                 url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
             }
